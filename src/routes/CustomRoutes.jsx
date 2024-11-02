e3205b75616eea3758c7b40499590b389e853f8b
Oyatillo12 <fayzillo998800@gmail.com>
2024-11-02 21:19:51 +0500

    loading added

--- src/routes/CustomRoutes.jsx
+++ src/routes/CustomRoutes.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home, Liked, Search, Single } from '../pages'
+import {  Liked, Search, Single } from '../pages'
 import {PATH} from '../hooks/usePath'
+
+const Home = lazy(() => new Promise(resolve => {
+  return setTimeout(() => resolve(import('../pages/Dashboard/Home')), 1000)
+}))
 
 function CustomRoutes() {
   return (
     <Routes>
-        <Route path={PATH.home} element={<Home />} />
+        <Route path={PATH.home} element={<Suspense fallback={<div className='h-[100vh] flex items-center justify-center login-wrapper'><span className='loader !scale-150'></span></div>}><Home/></Suspense>} />
         <Route path={PATH.search} element={<Search />} />
         <Route path={PATH.liked} element={<Liked />} />
         <Route path={PATH.single} element={<Single />} />
